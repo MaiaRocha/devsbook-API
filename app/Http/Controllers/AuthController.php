fc1beffadbb6d89275351e126ec3c49879347901
Romaria Rocha <maia.rocha1992@hotmail.com>
2022-01-27 12:59:34 -0300

    Adicionando as funções de criar usuário, login, logout,setar avatar e cover.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -18,6 +18,49 @@ class AuthController extends Controller
         ]);
     }
 
+    public function unauthorized() {
+        return response()->json(['error' => 'Não autorizado'], 401);
+    }
+
+    public function login(Request $request) {
+        $array = ['error' => ''];
+
+        $email = $request->input('email');
+        $password = $request->input('password');
+
+        if($email && $password) {
+
+        $token = auth()->attempt([
+            'email' => $email,
+            'password' => $password
+        ]);
+
+        if(!$token) {
+            $array['error'] = 'E-mail e/ou senha errados!';
+            return $array;
+        }
+
+        $array['token'] = $token;
+        return $array;
+        } 
+           
+        $array['error'] = 'Dados não enviados.';
+        return $array; 
+    }
+
+    public function logout() {
+        auth()->logout();
+        return ['error'=>''];
+    }
+
+    public function refresh() {
+        $token = auth()->refresh();
+        return [
+            'error' => '',
+            'token' => $token
+        ];
+    }
+
     public function create(Request $request) {
 
         $array = ['error' =>''];
@@ -34,7 +77,9 @@ class AuthController extends Controller
                 return $array;
             }
             //Verificar a existencia do email
+            
             $emailExists = User::where('email', $email)->count();
+            
             if($emailExists === 0) {
 
                 $hash = password_hash($password, PASSWORD_DEFAULT);
